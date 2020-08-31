f09fb2f0ec69f30ed03be43bbea08da3ad132d17
unknown <yorkzz1qz@163.com>
2020-08-31 16:03:20 +0800

    精简文件

--- redis_test.py
+++
@@ -1,7 +0,0 @@
-import redis
-#创建连接池
-pool = redis.ConnectionPool(host='127.0.0.1',port=6379,decode_responses=True)
-#创建链接对象
-r=redis.Redis(connection_pool=pool)
-r.set('test','dddddddddddd',ex=3,nx=True)
-print(r.get('test'))
